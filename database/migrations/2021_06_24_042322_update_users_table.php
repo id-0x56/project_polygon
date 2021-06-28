73a07deb0e07277e35f7031e19e3db78d23f21e5
id-0x56 <id.0x56@Gmail.com>
2021-06-28 05:39:05 +0300

    last login event

---
+++ database/migrations/2021_06_24_042322_update_users_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->datetime('last_login_at')->after('profile_photo_path')->nullable();
+            $table->string('last_login_ip')->after('last_login_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('last_login_at', 'last_login_ip');
+        });
+    }
+}
