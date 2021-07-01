b84d71b1b9cffcf7deb3b07544b91c412cab54c9
id-0x56 <id.0x56@Gmail.com>
2021-07-01 12:04:54 +0300

    add user & role relationships

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Role extends Model
 {
     use HasFactory;
+
+    public function users()
+    {
+        return $this->hasMany(User::class);
+    }
+
+    public function posts()
+    {
+        return $this->hasManyThrough(Post::class, User::class);
+    }
 }
